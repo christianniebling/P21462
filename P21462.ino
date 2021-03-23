3a9ea0508bf17be84b88c43ff4a494b26863d3d6
Christian Niebling <christian.v.niebling@gmail.com>
2021-03-23 14:18:50 -0400

    updates

--- P21462.ino
+++ P21462.ino
@@ -12,6 +12,8 @@
 #define LED_COUNT 28
 
 #define CURRENT_PIN_CC  A2
+#define CURRENT_PIN_BATT A0
+#define VOLTAGE_PIN_BATT A3
 
 
 Adafruit_7segment segment1 = Adafruit_7segment();
@@ -29,16 +31,13 @@ struct sensor {
   double power;
 };
 
-struct sensor sensor1;
+struct sensor charge;
+struct sensor battery;
 
 int displayValue = 432;
-double d1 = 0.123;
-double d2 = 0.456;
-double d3 = 0.789;
 
 const float FACTOR = 40.0/1000;
 const float QOV = 0.5*5.0;
-float voltage;
 
 void setup() {
   // put your setup code here, to run once:
@@ -61,19 +60,17 @@ void setup() {
   
   //segment1.print(displayValue, DEC);
   //segment1.printError();
-  segment1.printFloat(d1,3);
-  segment2.printFloat(d2,3);
-  segment3.printFloat(d3,3);
-  
-  segment1.writeDisplay();
-  segment2.writeDisplay();
-  segment3.writeDisplay();
+
 
   lcd1.setCursor(0,0);
-  lcd1.print("hello team P21462");
+  lcd1.print("battery voltage:");
+  lcd1.setCursor(0,2);
+  lcd1.print("battery current:");
 
   lcd2.setCursor(0,0);
-  lcd2.print("greetings team P21462");
+  lcd2.print("CC voltage:");
+  lcd2.setCursor(0,2);
+  lcd2.print("CC current:");
 
 
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
@@ -84,33 +81,92 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+// put your main code here, to run repeatedly:
 //  sensor1.current = analogRead(A0);
 //  sensor1.voltage = analogRead(A1);
 //  sensor1.current = MyMap(sensor1.current, 0, 1023, 0, 50); //0-50 range for 50-amp sensor
 //  sensor1.voltage = MyMap(sensor1.voltage, 0, 1023, 0, 12); 
 //  computePower(sensor1);
 
-  float raw_voltage = (5.0/1023.0)*analogRead(CURRENT_PIN_CC);
-  raw_voltage = raw_voltage - QOV + 0.007;
-  voltage = raw_voltage / FACTOR;
-  segment2.printFloat(voltage, 3);
-  segment2.writeDisplay();
-  Serial.println(voltage,3);
-  
-  delay(500);
-  //rainbow(1);             // Flowing rainbow cycle along the whole strip
+  battery.voltage = readVoltage(VOLTAGE_PIN_BATT,16);
+  battery.voltage -= 0.4; //manual compensation for incorrect resistor
+  battery.current = readCurrent(CURRENT_PIN_BATT);
+  battery.power = computePower(battery);
 
+  segment1.printFloat(battery.voltage,3);
+  segment2.printFloat(battery.current,3);
+  segment3.printFloat(battery.power,3);
+  segment1.writeDisplay();
+  segment2.writeDisplay();
+  segment3.writeDisplay();
+  
+  charge.current = readCurrent(CURRENT_PIN_CC);
+//  segment2.printFloat(charge.current, 3);
+//  segment2.writeDisplay();
+  //Serial.println(charge.current,3);
+  lcd2.setCursor(0,1);
+  lcd2.print("---");
+  lcd2.setCursor(0,3);
+  lcd2.print(charge.current);
+
+  lcd1.setCursor(0,1);
+  lcd1.print(battery.voltage);
+  lcd1.setCursor(0,3);
+  lcd1.print(battery.current);
+  
+  disp_batt_level();
+
+ delay(500);
 }
 
-void computePower(struct sensor sens)
+float computePower(struct sensor sens)
 {
-  sens.power = sens.voltage * sens.current;
+  return sens.voltage * sens.current;
+}
+
+float readCurrent(int pin)
+{
+  float raw_voltage = (5.0/1023.0)*analogRead(pin);
+  float voltage = raw_voltage - QOV;
+  float current = voltage / FACTOR;
+  return current;
+}
+
+float readVoltage(int pin, float maximum)
+{
+  float input = analogRead(pin); //number from 0 to 1023 based on input voltage
+  float voltage = MyMap(input,0,1023,0,maximum);
+  return voltage;
+  
 }
 
 //maping function that does not truncate decimal (old map() type casts to interger format, here we use double)
 double MyMap(double x, int in_min, int in_max, int out_min, int out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void disp_batt_level()//function to display battery level on the lower left neopixel bar
+{
+  int len = strip.numPixels()/2;
+  int num_lit = map(battery.voltage,0,16,0,len);
+  int R = 255, G = 0, B = 0;
+  int interval = 255/len;
+  int counter = 0;
+  for(int i = LED_COUNT-1; i > len; i--)
+  {
+    if(counter < num_lit)
+    {
+      strip.setPixelColor(i,R,G,B);
+      R -= interval;
+      G += interval;
+    }
+    else
+    {
+      strip.setPixelColor(i,0,0,0);
+    }
+    counter++;
+  }
+  strip.show();
 }
 
 // Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
